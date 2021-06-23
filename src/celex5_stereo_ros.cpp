9920f23f12a0b73a5f9a950da7b715a765b0d03e
robin_shaun <robin_shaun@foxmail.com>
2021-06-23 13:33:21 +0800

    update for full picture

--- src/celex5_stereo_ros.cpp
+++ src/celex5_stereo_ros.cpp
@@ -4,29 +4,34 @@ class Celex5StereoRos : public CeleX5DataManager {
 public:
   std::vector<EventData> vecEvent_;
   cv::Mat event_frame_;
+  cv::Mat full_picture_;
 
-  pthread_mutex_t frame_lock = PTHREAD_MUTEX_INITIALIZER;
+  pthread_mutex_t event_frame_lock = PTHREAD_MUTEX_INITIALIZER;
   pthread_mutex_t event_vec_lock = PTHREAD_MUTEX_INITIALIZER;
+  pthread_mutex_t full_picture_lock = PTHREAD_MUTEX_INITIALIZER;
 
   CX5SensorDataServer *m_pServer_;
   CeleX5 *celex_;
 
+  CeleX5::emEventPicType type_;
+
   Celex5StereoRos(CX5SensorDataServer *pServer){
-    // pthread_mutex_init(&frame_lock,NULL);
+    // pthread_mutex_init(&event_frame_lock,NULL);
     // pthread_mutex_init(&event_vec_lock,NULL);
     m_pServer_ = pServer;
     m_pServer_->registerData(this, CeleX5DataManager::CeleX_Frame_Data);
   }
 
   ~Celex5StereoRos() {
-    pthread_mutex_destroy(&frame_lock);
+    pthread_mutex_destroy(&event_frame_lock);
     pthread_mutex_destroy(&event_vec_lock);
     m_pServer_->unregisterData(this, CeleX5DataManager::CeleX_Frame_Data);
     delete celex_;
   }
 
   std::vector<EventData> getEventVector();
-  cv::Mat getFrame();
+  cv::Mat getEventFrame();
+  cv::Mat getFullPicture();
   // overrides the update operation
   virtual void onFrameDataUpdated(CeleX5ProcessedData *pSensorData);
 };
@@ -37,19 +42,30 @@ void Celex5StereoRos::onFrameDataUpdated(
     CeleX5ProcessedData *pSensorData) {
 
   // get event_frame
-  pthread_mutex_lock(&frame_lock);
-  event_frame_ = cv::Mat(800, 1280, CV_8UC1, pSensorData->getEventPicBuffer(CeleX5::EventBinaryPic));
-  pthread_mutex_unlock(&frame_lock);
+  pthread_mutex_lock(&event_frame_lock);
+  event_frame_ = cv::Mat(800, 1280, CV_8UC1, pSensorData->getEventPicBuffer(type_));
+  pthread_mutex_unlock(&event_frame_lock);
   // get raw event data
   pthread_mutex_lock(&event_vec_lock);
   vecEvent_ = pSensorData->getEventDataVector();
   pthread_mutex_unlock(&event_vec_lock);
+  // get full_picture
+  pthread_mutex_lock(&full_picture_lock);
+  full_picture_ = cv::Mat(800, 1280, CV_8UC1, pSensorData->getFullPicBuffer());
+  pthread_mutex_unlock(&full_picture_lock);
   }
 
-cv::Mat Celex5StereoRos::getFrame(){
-  pthread_mutex_lock(&frame_lock);
+cv::Mat Celex5StereoRos::getFullPicture(){
+  pthread_mutex_lock(&event_frame_lock);
+  auto tmp=this->full_picture_.clone(); //clone为深拷贝
+  pthread_mutex_unlock(&event_frame_lock);
+  return tmp; //mat有自己的数据管理，拷贝代价低
+}
+
+cv::Mat Celex5StereoRos::getEventFrame(){
+  pthread_mutex_lock(&event_frame_lock);
   auto tmp=this->event_frame_.clone(); //clone为深拷贝
-  pthread_mutex_unlock(&frame_lock);
+  pthread_mutex_unlock(&event_frame_lock);
   return tmp; //mat有自己的数据管理，拷贝代价低
 }
 
@@ -80,7 +96,7 @@ int threshold, clock_rate, event_frame_time;
 
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "celex_ros_callback");
+  ros::init(argc, argv, "celex_ros");
   ros::NodeHandle node_;
   CeleX5 *pCelex_;
   pCelex_ = new CeleX5;
@@ -89,49 +105,89 @@ int main(int argc, char **argv) {
   pCelex_->openSensor(CeleX5::CeleX5_MIPI);
 
   publisher_with_id events_pub_0;
-  publisher_with_id events_pub_1;
   publisher_with_id event_frame_pub_0;
+  publisher_with_id full_picture_pub_0;
+  publisher_with_id events_pub_1;
   publisher_with_id event_frame_pub_1;
+  publisher_with_id full_picture_pub_1;
 
   // grab the parameters
-  node_.param<std::string>("/celex_ros_callback/celex_mode", celex_mode,"Event_Address_Only_Mode");
-  node_.param<std::string>("/celex_ros_callback/event_pic_type", event_pic_type,"EventBinaryPic");
-  node_.param<int>("/celex_ros_callback/threshold", threshold, 170);   // 0-1024
-  node_.param<int>("/celex_ros_callback/clock_rate", clock_rate, 100); // 0-100
-  node_.param<int>("/celex_ros_callback/event_frame_time", event_frame_time, 1e4); // 0-100
+  node_.param<std::string>("/celex_ros/celex_mode", celex_mode,"Event_Address_Only_Mode");
+  node_.param<std::string>("/celex_ros/event_pic_type", event_pic_type,"EventBinaryPic");
+  node_.param<int>("/celex_ros/threshold", threshold, 170);   // 0-1024
+  node_.param<int>("/celex_ros/clock_rate", clock_rate, 100); // 0-100
+  node_.param<int>("/celex_ros/event_frame_time", event_frame_time, 1e4); // 0-100
 
   events_pub_0.pub = node_.advertise<celex5_msgs::eventVector>("celex5_0/events", 10);
   events_pub_0.id = 0;
   events_pub_0.node = node_;
   events_pub_0.event_frame_time = event_frame_time;
-  events_pub_1.pub  = node_.advertise<celex5_msgs::eventVector>("celex5_1/events", 10);
-  events_pub_1.id = 1;
-  events_pub_1.node = node_;
-  events_pub_1.event_frame_time = event_frame_time;
   event_frame_pub_0.pub = node_.advertise<sensor_msgs::Image>("celex5_0/event_frame", 10);
   event_frame_pub_0.id = 0;
   event_frame_pub_0.node = node_;
   event_frame_pub_0.event_frame_time = event_frame_time;
+  full_picture_pub_0.pub = node_.advertise<sensor_msgs::Image>("celex5_0/full_picture", 10);
+  full_picture_pub_0.id = 0;
+  full_picture_pub_0.node = node_;
+  full_picture_pub_0.event_frame_time = event_frame_time;
+  events_pub_1.pub  = node_.advertise<celex5_msgs::eventVector>("celex5_1/events", 10);
+  events_pub_1.id = 1;
+  events_pub_1.node = node_;
+  events_pub_1.event_frame_time = event_frame_time;
   event_frame_pub_1.pub = node_.advertise<sensor_msgs::Image>("celex5_1/event_frame", 10);
   event_frame_pub_1.id = 1;
   event_frame_pub_1.node = node_;
   event_frame_pub_1.event_frame_time = event_frame_time;
+  full_picture_pub_1.pub = node_.advertise<sensor_msgs::Image>("celex5_1/full_picture", 10);
+  full_picture_pub_1.id = 1;
+  full_picture_pub_1.node = node_;
+  full_picture_pub_1.event_frame_time = event_frame_time;
 
-  pCelex_->setThreshold(threshold,0);
+  pCelex_->setThreshold(threshold, 0);
   pCelex_->setThreshold(threshold, 1);
-  pCelex_->setEventFrameTime(event_frame_time,0);
-  pCelex_->setEventFrameTime(event_frame_time,1);
+  pCelex_->setEventFrameTime(event_frame_time, 0);
+  pCelex_->setEventFrameTime(event_frame_time, 1);
 
   CeleX5::CeleX5Mode mode;
   if (celex_mode == "Event_Address_Only_Mode")
     mode = CeleX5::Event_Address_Only_Mode;
   else if (celex_mode == "Event_Optical_Flow_Mode")
     mode = CeleX5::Event_Optical_Flow_Mode;
+  else if (celex_mode == "Event_Intensity_Mode")
+    mode = CeleX5::Event_Intensity_Mode;
+  else if (celex_mode == "Full_Picture_Mode")
+    mode = CeleX5::Full_Picture_Mode;
+  else if (celex_mode == "Full_Optical_Flow_S_Mode")
+    mode = CeleX5::Full_Optical_Flow_S_Mode;
+  else if (celex_mode == "Full_Optical_Flow_M_Mode")
+    mode = CeleX5::Full_Optical_Flow_M_Mode;
   pCelex_->setSensorFixedMode(mode, 0);
   pCelex_->setSensorFixedMode(mode, 1);
 
+  CeleX5::emEventPicType type;
+  if (event_pic_type == "EventBinaryPic")
+    type = CeleX5::EventBinaryPic;
+  else if (event_pic_type == "EventAccumulatedPic")
+    type = CeleX5::EventAccumulatedPic;
+  else if (event_pic_type == "EventGrayPic")
+    type = CeleX5::EventGrayPic;
+  else if (event_pic_type == "EventCountPic")
+    type = CeleX5::EventCountPic;
+  else if (event_pic_type == "EventDenoisedBinaryPic")
+    type = CeleX5::EventDenoisedBinaryPic;
+  else if (event_pic_type == "EventSuperimposedPic")
+    type = CeleX5::EventSuperimposedPic;
+  else if (event_pic_type == "EventDenoisedCountPic")
+    type = CeleX5::EventDenoisedCountPic;
+
+  pCelex_->setFpnFile("/home/robin/catkin_ws/src/celex5_stereo/celex5dependencies/stereo/config/FPN_M_2.txt", 0);
+  pCelex_->setFpnFile("/home/robin/catkin_ws/src/celex5_stereo/celex5dependencies/stereo/config/FPN_S_2.txt", 1);
+
   celex_ros_event[0] = new Celex5StereoRos(pCelex_->getSensorDataServer(0));
-  celex_ros_event[1]= new Celex5StereoRos(pCelex_->getSensorDataServer(1));
+  celex_ros_event[1] = new Celex5StereoRos(pCelex_->getSensorDataServer(1));
+
+  celex_ros_event[0]->type_ = type;
+  celex_ros_event[1]->type_ = type;
 
   // events[0] = celex_ros_0->vecEvent_;
   // events[1] = celex_ros_1->vecEvent_;
@@ -140,11 +196,12 @@ int main(int argc, char **argv) {
   
   pthread_t events_pub_thread_0;
   pthread_t events_pub_thread_1;
-  pthread_t event_frame_pub_thread_0;
-  pthread_t event_frame_pub_thread_1;   
+  pthread_t frame_pub_thread_0;
+  pthread_t frame_pub_thread_1;   
 
   int rc;
   void *publish_event_frame(void *arg);
+  void *publish_full_picture(void *arg);
   void *publish_events(void *arg);
 
   cout << "main() : 创建主相机事件向量发布线程" << endl;      
@@ -153,11 +210,22 @@ int main(int argc, char **argv) {
       cout << "Error:无法创建主相机事件向量发布线程" << endl;
       exit(-1);
   }
-  cout << "main() : 创建主相机事件帧发布线程" << endl;      
-  rc = pthread_create(&event_frame_pub_thread_0, NULL, publish_event_frame, (void *)&event_frame_pub_0);
-  if (rc){
-      cout << "Error:无法创建主相机事件帧发布线程" << endl;
-      exit(-1);
+
+  if (mode == CeleX5::Event_Address_Only_Mode){
+    cout << "main() : 创建主相机事件帧发布线程" << endl;      
+    rc = pthread_create(&frame_pub_thread_0, NULL, publish_event_frame, (void *)&event_frame_pub_0);
+    if (rc){
+        cout << "Error:无法创建主相机事件帧发布线程" << endl;
+        exit(-1);
+    }
+  }
+  else if (mode == CeleX5::Full_Picture_Mode){
+    cout << "main() : 创建主相机灰度图发布线程" << endl;      
+    rc = pthread_create(&frame_pub_thread_0, NULL, publish_full_picture, (void *)&full_picture_pub_0);
+    if (rc){
+        cout << "Error:无法创建主相机灰度图发布线程" << endl;
+        exit(-1);
+    }
   }
 
   cout << "main() : 创建从相机事件向量发布线程" << endl;      
@@ -166,12 +234,24 @@ int main(int argc, char **argv) {
       cout << "Error:无法创建主相机事件向量发布线程" << endl;
       exit(-1);
   }
-  cout << "main() : 创建从相机事件帧发布线程, " << endl;      
-  rc = pthread_create(&event_frame_pub_thread_1, NULL, publish_event_frame, (void *)&event_frame_pub_1);
-  if (rc){
-      cout << "Error:无法创建主相机事件帧发布线程," << endl;
-      exit(-1);
+
+  if (mode == CeleX5::Event_Address_Only_Mode){
+    cout << "main() : 创建从相机事件帧发布线程, " << endl;      
+    rc = pthread_create(&frame_pub_thread_1, NULL, publish_event_frame, (void *)&event_frame_pub_1);
+    if (rc){
+        cout << "Error:无法创建主相机事件帧发布线程," << endl;
+        exit(-1);
+    }
   }
+  else if (mode == CeleX5::Full_Picture_Mode){
+    cout << "main() : 创建从相机灰度图发布线程" << endl;      
+    rc = pthread_create(&frame_pub_thread_1, NULL, publish_full_picture, (void *)&full_picture_pub_1);
+    if (rc){
+        cout << "Error:无法创建从相机灰度图发布线程" << endl;
+        exit(-1);
+    }
+  }
+
   ros::Rate loop_rate(1e6/event_frame_time);
   while (node_.ok()) {
     // events[0] = celex_ros_0->vecEvent_;
@@ -189,10 +269,24 @@ void *publish_event_frame(void *arg){
     publisher = (struct publisher_with_id *) arg;
     ros::Rate loop_rate(1e6/publisher->event_frame_time);
   while (publisher->node.ok()) {
-    auto data=celex_ros_event[publisher->id]->getFrame();
+    auto data=celex_ros_event[publisher->id]->getEventFrame();
     sensor_msgs::ImagePtr event_frame_ros = cv_bridge::CvImage(std_msgs::Header(), "mono8", data).toImageMsg();
     // sensor_msgs::ImagePtr event_frame_ros = cv_bridge::CvImage(std_msgs::Header(), "mono8", event_frame[publisher->id]).toImageMsg();
     publisher->pub.publish(event_frame_ros);
+    // ros::spinOnce();
+    loop_rate.sleep();
+  }
+}
+
+void *publish_full_picture(void *arg){
+    struct publisher_with_id *publisher;
+    publisher = (struct publisher_with_id *) arg;
+    ros::Rate loop_rate(1e6/publisher->event_frame_time);
+  while (publisher->node.ok()) {
+    auto data=celex_ros_event[publisher->id]->getFullPicture();
+    sensor_msgs::ImagePtr full_picture_ros = cv_bridge::CvImage(std_msgs::Header(), "mono8", data).toImageMsg();
+    // sensor_msgs::ImagePtr event_frame_ros = cv_bridge::CvImage(std_msgs::Header(), "mono8", event_frame[publisher->id]).toImageMsg();
+    publisher->pub.publish(full_picture_ros);
     // ros::spinOnce();
     loop_rate.sleep();
   }
